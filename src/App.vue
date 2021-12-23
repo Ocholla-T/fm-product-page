4e37df33af623022a59c6e6616c0ca8c79a34e23
Ocholla-T <tonnyocholla8@gmail.com>
2021-12-23 19:42:01 +0300

    linked navigationBar component to page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
-<template></template>
+<template>
+  <NavigationBar />
+</template>
 
-<script setup lang="ts"></script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup lang="ts">
+import NavigationBar from './components/NavigationBar.vue';
+</script>
